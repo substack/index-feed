2da9fc4eca18dba3c0283f235f874ba37e9e7f9f
James Halliday <mail@substack.net>
2015-02-01 02:43:15 -0800

    levelup-defaults trickery

--- example/feed.js
+++ example/feed.js
@@ -1,15 +1,16 @@
 var ixfeed = require('../');
+var through = require('through2');
 var level = require('level');
 
-var ddb = level(__dirname + '/data.db', { valueEncoding: 'json' });
+var ddb = level(__dirname + '/data.db');
 var idb = level(__dirname + '/index.db');
+var ixf = ixfeed({ data: ddb, index: idb, valueEncoding: 'json' });
 
-var ixf = ixfeed();
 ixf.index.add(function (row, cb) {
-    if (/^user!/.test(row.key)) {
+    if (/^hacker!/.test(row.key)) {
         cb(null, {
-            'user.name': row.value.name,
-            'user.space': row.value.hackerspace
+            'hacker.name': row.value.name,
+            'hacker.space': row.value.hackerspace
         });
     }
     else cb()
@@ -22,25 +23,25 @@ ixf.feed.createReadStream({ live: true })
 ixf.db.batch([
     {
         type: 'put',
-        key: 'user!1',
+        key: 'hacker!1',
         value: { name: 'substack', hackerspace: 'sudoroom' }
     },
     {
         type: 'put',
-        key: 'user!2',
+        key: 'hacker!2',
         value: { name: 'mk30', hackerspace: 'sudoroom' }
     },
     {
         type: 'put',
-        key: 'user!3',
+        key: 'hacker!3',
         value: { name: 'mitch', hackerspace: 'noisebridge' }
     }
 ], ready);
 
 function ready () {
-    var sudoroom = ixf.indexes.createReadStream('user.space', {
-        lte: 'sudoroom',
-        gte: 'sudoroom'
+    // list all hackers at sudoroom:
+    var sudoroom = ixf.index.createReadStream('hacker.space', {
+        lte: 'sudoroom', gte: 'sudoroom'
     });
     sudoroom.pipe(through.obj(function (row, enc, next) {
         console.log(row.value.name);
